9b514aea7b6a8022cd8e282e81e74b791cc2799d
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2024-07-24 07:42:23 -0400

    Added dts to build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" }),
+  ],
   build: {
     rollupOptions: {
       // https://rollupjs.org/guide/en/#big-list-of-options
