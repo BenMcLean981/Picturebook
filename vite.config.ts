03f4aff49abc40360bfb93c68d078909e245a4a3
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2024-07-25 22:52:37 -0400

    Got ready for initial release.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import { extname, relative, resolve } from "path";
+import { glob } from "glob";
+import { fileURLToPath } from "url";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" }),
+    dts({ tsconfigPath: "./tsconfig.app.json", rollupTypes: true }),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+    },
     rollupOptions: {
-      // https://rollupjs.org/guide/en/#big-list-of-options
-      input: "src/index.ts",
-      output: [
-        {
-          format: "cjs",
-          entryFileNames: "index.js",
-        },
-      ],
+      external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob
+          .sync("src/**/*.{ts,tsx}", { ignore: ["src/**/*.d.ts"] })
+          .map((file) => [
+            relative("src", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
     },
   },
 });
